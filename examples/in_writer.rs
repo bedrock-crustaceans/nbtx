f4d613e5c07a6b9c887b38be83a382c3571a4a57
theaddon <theaddonn@gmail.com>
2024-10-09 23:45:28 +0200

    fix in_writer example

--- examples/in_writer.rs
+++ examples/in_writer.rs
@@ -1,12 +1,12 @@
 use std::io::Write;
-use byteorder::WriteBytesExt;
+use byteorder::{BigEndian, LittleEndian, WriteBytesExt};
 
 pub fn main() {
     let mut writer = Vec::new();
 
-    writer.write_u16(69).unwrap();
-    writer.write_u128(42).unwrap();
+    writer.write_u16::<LittleEndian>(69).unwrap();
+    writer.write_u128::<BigEndian>(42).unwrap();
     // You can write a nbt into an existing writer without any problems!
-    nbtx::to_bytes_in(&mut writer, &vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).unwrap();
+    nbtx::to_bytes_in::<LittleEndian>(&mut writer, &vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).unwrap();
     writer.write_all("Hellooo nbtx!".as_bytes()).unwrap();
 }
