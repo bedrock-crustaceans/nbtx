fe4588e1d000278d6b788103bae5b35731993597
Lucy <theaddonn@gmail.com>
2025-01-26 16:04:50 +0100

    Update comments related to Hashing in value.rs

--- src/value.rs
+++ src/value.rs
@@ -401,22 +401,23 @@ impl Hash for Value {
             Value::Long(v) => state.write_i64(*v),
             Value::String(v) => state.write(v.as_bytes()),
             Value::Float(v) => {
-                // IEEE floats have - and + 0 which are the same value but have different bytes meaning you need a fallback like this
-                let bytes = if *v == (-0f32) {
-                    0f32.to_le_bytes()
+                // f32 does not implement Hash, so simply hash the byte representation
+                // IEEE floats have + 0 and + 0, which are the same value but have different byte representations
+                let bytes = if *v == (-0_f32) {
+                    0_f32.to_le_bytes()
                 } else {
                     v.to_le_bytes()
                 };
-                // f32 does not implement Hash so simply hash the byte repr.
                 state.write(&bytes);
             }
             Value::Double(v) => {
-                let bytes = if *v == (-0f64) {
-                    0f64.to_le_bytes()
+                // f32 does not implement Hash, so simply hash the byte representation
+                // IEEE floats have + 0 and + 0, which are the same value but have different byte representations
+                let bytes = if *v == (-0_f64) {
+                    0_f64.to_le_bytes()
                 } else {
                     v.to_le_bytes()
                 };
-                // f64 does not implement Hash so simply hash the byte repr.
                 state.write(&bytes);
             }
             Value::Compound(map) => {
